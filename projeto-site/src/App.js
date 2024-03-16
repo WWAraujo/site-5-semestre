1f4487c8ed613e885b562b5adf2825090a54ed14
Wallace Wagner <wallace20130@gmail.com>
2024-03-16 11:33:31 -0300

    add botão whats

--- projeto-site/src/App.js
+++ projeto-site/src/App.js
@@ -1,20 +1,34 @@
 import logo from './imagem/logo-escuro.png';
 import fotoInicio from './imagem/001.JPG'
 import './App.css';
+import { WhatsAppWidget } from 'react-whatsapp-widget';
+import 'react-whatsapp-widget/dist/index.css';
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img className="App-logo" src={logo}alt="Logo da Arte Vieira"/>
       </header>
+
       <main className="App-content">
         <img className="App-foto-inicio" src={fotoInicio} 
         alt="Foto com 4 drinks diferentes, 1 negroni, 1 moscow mule, 1 maracujack, 1 passione, e atrás dos drinks 3 garrafas usadas para fazer os drinks, Vodka, Aperou e Gin" />
+
       </main>
+
       <footer className="App-footer">
         <h4 className="App-h4-footer">COPYRIGHT © 2024 | ARTE VIEIRA EVENTOS | TODOS OS DIREITOS RESERVADOS.</h4>
       </footer>
+
+      <div className="App">
+        <h1>Meu Site</h1>
+        <WhatsAppWidget 
+        phoneNumber="+5511959680087" message="Olá, como posso ajudar?"
+        />
+      </div>
+      
     </div>
   );
 }
